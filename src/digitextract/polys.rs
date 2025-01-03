61f4608ab21b41d3b9da9a2a513e745b3e43bab0
Simon Pohmann <simon@pohmann.de>
2025-01-03 15:06:25 +0000

    Added precomputed digit extraction circuit for 2^23

--- src/digitextract/polys.rs
+++ src/digitextract/polys.rs
@@ -24,34 +24,64 @@ use crate::digitextract::ArithCircuit;
 /// `e` must be a power of two.
 /// 
 /// Uses a lookup-table, consisting mainly of the values from [https://ia.cr/2022/1364], except for
-/// `e = 16`, where there seemed to be a mistake in the paper.
+/// `e > 8`, where there seemed to be a mistake in the paper.
 /// 
 pub fn precomputed_p_2(e: usize) -> ArithCircuit {
-    let log2_e = StaticRing::<i64>::RING.abs_log2_ceil(&(e as i64)).unwrap();
-    assert_eq!(e, 1 << log2_e);
+    assert!(e <= 23, "no precomputed tables are available for t > 2^23");
+    let log2_e_ceil = StaticRing::<i64>::RING.abs_log2_ceil(&(e as i64)).unwrap();
     
     let id = ArithCircuit::linear_transform(&[1]);
     let f0 = id.clone();
     let f1 = id.tensor(&ArithCircuit::mul()).compose(&ArithCircuit::select(1, &[0, 0, 0]).compose(&f0));
     let f2 = id.tensor(&id).tensor(&ArithCircuit::mul()).compose(&ArithCircuit::select(2, &[0, 1, 1, 1]).compose(&f1));
     
-    let f3_comp = ArithCircuit::add().compose(&ArithCircuit::linear_transform(&[112]).tensor(
-        &ArithCircuit::mul().compose(&ArithCircuit::linear_transform(&[94, 121]).output_twice())
-    )).compose(&ArithCircuit::select(2, &[0, 0, 1]));
-    let f3 = id.tensor(&id).tensor(&id).tensor(&f3_comp).compose(&ArithCircuit::select(3, &[0, 1, 2, 1, 2]).compose(&f2));
+    let f3_comp = ArithCircuit::add().compose(
+        &ArithCircuit::linear_transform(&[112]).tensor(&ArithCircuit::mul().compose(
+            &ArithCircuit::linear_transform(&[94, 121]).output_twice()
+        ))
+    ).compose(
+        &ArithCircuit::select(2, &[0, 0, 1])
+    );
+    let f3 = id.tensor(&id).tensor(&id).tensor(&f3_comp).compose(
+        &ArithCircuit::select(3, &[0, 1, 2, 1, 2])
+    ).compose(
+        &f2
+    );
 
-    let f4_comp = ArithCircuit::add().compose(&ArithCircuit::linear_transform(&[1984, 528, 22620]).tensor(
-        &ArithCircuit::mul().compose(&ArithCircuit::linear_transform(&[226, 113]).tensor(&ArithCircuit::linear_transform(&[8, 2, 301])))
-    )).compose(&ArithCircuit::select(3, &[0, 1, 2, 1, 2, 0, 1, 2]));
-    let f4 = id.tensor(&id).tensor(&id).tensor(&id).tensor(&f4_comp).compose(&ArithCircuit::select(4, &[0, 1, 2, 3, 1, 2, 3]).compose(&f3));
+    let f4_comp = ArithCircuit::add().compose(
+        &ArithCircuit::linear_transform(&[1984, 528, 22620]).tensor(&ArithCircuit::mul().compose(
+            &ArithCircuit::linear_transform(&[226, 113]).tensor(&ArithCircuit::linear_transform(&[8, 2, 301]))
+        ))
+    ).compose(
+        &ArithCircuit::select(3, &[0, 1, 2, 1, 2, 0, 1, 2])
+    );
+    let f4 = id.tensor(&id).tensor(&id).tensor(&id).tensor(&f4_comp).compose(
+        &ArithCircuit::select(4, &[0, 1, 2, 3, 1, 2, 3])
+    ).compose(
+        &f3
+    );
 
-    return match log2_e {
+    let f5_comp = ArithCircuit::add().compose(
+        &ArithCircuit::linear_transform(&[4849408, 3564625, 2737008, 6563608]).tensor(&ArithCircuit::mul().compose(
+            &ArithCircuit::linear_transform(&[997183, 8295548, 419894, 879825]).tensor(&ArithCircuit::linear_transform(&[443729, 555132, 491350, 758385]))
+        ))
+    ).compose(
+        &ArithCircuit::select(4, &[0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+    );
+    let f5 = id.tensor(&id).tensor(&id).tensor(&id).tensor(&id).tensor(&f5_comp).compose(
+        &ArithCircuit::select(5, &[0, 1, 2, 3, 4, 1, 2, 3, 4])
+    ).compose(
+        &f4
+    );
+
+    return match log2_e_ceil {
         0 => f0,
         1 => f1,
         2 => f2,
         3 => f3,
         4 => f4,
-        5.. => panic!("no table entry for {}", e)
+        5 => f5,
+        6.. => unreachable!()
     };
 }
 
@@ -318,13 +348,26 @@ use feanor_math::assert_el_eq;
 use feanor_math::rings::finite::FiniteRingStore;
 
 #[test]
+#[ignore]
+fn test_digit_extraction_p_2_complete() {
+    let circuit = precomputed_p_2(23);
+    let ring = Zn::new(1 << 23);
+    let hom = ring.can_hom(&StaticRing::<i64>::RING).unwrap();
+    for x in 0..(1 << 23) {
+        for (e, actual) in [1, 2, 4, 8, 16, 23].into_iter().zip(circuit.evaluate(&[hom.map(x)], &ring)) {
+            assert_eq!(x % 2, ring.smallest_positive_lift(actual) % (1 << e));
+        }
+    }
+}
+
+#[test]
 fn test_digit_extraction_p_2() {
     let circuit = precomputed_p_2(16);
     let ring = Zn::new(1 << 16);
     let hom = ring.can_hom(&StaticRing::<i64>::RING).unwrap();
     for x in 0..(1 << 16) {
-        for (i, actual) in (0..=4).zip(circuit.evaluate(&[hom.map(x)], &ring)) {
-            assert_eq!(x % 2, ring.smallest_positive_lift(actual) % (1 << (1 << i)));
+        for (e, actual) in [1, 2, 4, 8, 16].into_iter().zip(circuit.evaluate(&[hom.map(x)], &ring)) {
+            assert_eq!(x % 2, ring.smallest_positive_lift(actual) % (1 << e));
         }
     }
 }
